18ce446016c6b10daf3dd100200e4a2f7edf7f8f
amruta <amruta30nb@gmail.com>
2024-10-15 13:19:41 +0200

    -- Added Docker compose file --Added credentials to env file -- Updated READMe.md file

--- server/settings/develop.py
+++ server/settings/develop.py
@@ -1,14 +1,20 @@
 """
 MySQL database credentials
 """
+import os
+from dotenv import load_dotenv
+from .settings import BASE_DIR
+
+env_path = BASE_DIR + '/.env'  # If your .env is in the config folder
+load_dotenv(dotenv_path=env_path)
 
 SQL_DETAILS = {
     # 'SQL_USER' : os.environ.get("JWT_RESOURCE_ID"),
-    'USER': "task_user",
-    'PASSWORD': "Password123",
-    'HOST' : "localhost",
-    'DB_NAME' : "task_tracker",
-    'PORT': "3306",
+    'DB_NAME': os.getenv('MYSQL_DATABASE'),
+    'USER': os.getenv('MYSQL_USER'),
+    'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+    'HOST': os.getenv('MYSQL_HOST', 'mysql-db'),
+    'PORT': os.getenv('MYSQL_PORT', '3306'),
 
 }
 
