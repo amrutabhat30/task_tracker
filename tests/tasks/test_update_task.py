4d4f6220edac9cdb93888829f0d488a4e695124a
amruta <amruta30nb@gmail.com>
2024-10-21 13:07:39 +0200

    addedd screenshots for readme and added command to run pytests

--- tests/tasks/test_update_task.py
+++ tests/tasks/test_update_task.py
@@ -2,14 +2,15 @@
 import json
 
 class TestUpdateTask:
-    def test_update_task_success(self, client):
+    def test_update_task_success(self, client, create_task):
         """Test successful update of a task."""
         # Assume a task with ID 1 exists
+        task_id = create_task["id"]  # Use the ID of the created task
         task_data = {
             "title": "Updated Task",
             "description": "This is an updated test task."
         }
-        response = client.put("/api/tasks/23/", data=task_data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=task_data, content_type='application/json')
 
         assert response.status_code == 200  # HTTP 200 OK
         response_data = json.loads(response.content)
@@ -17,36 +18,39 @@ class TestUpdateTask:
         assert response_data['data']['title'] == task_data['title']
         assert response_data['data']['description'] == task_data['description']
 
-    def test_update_task_missing_required_fields(self, client):
+    def test_update_task_missing_required_fields(self, client, create_task):
         """Test case for when a required field (title) is missing."""
+        task_id = create_task["id"]
         data = {
             "description": "Missing title field"
         }
-        response = client.put("/api/tasks/26/", data=data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=data, content_type='application/json')
 
         assert response.status_code == 400
         assert "error" in response.json()  # Error details in 'error'
         assert "title" in response.json()["error"][0]["loc"]
 
-    def test_update_task_empty_fields(self, client):
+    def test_update_task_empty_fields(self, client, create_task):
         """Test case for when the 'title' field is empty."""
+        task_id = create_task["id"]
         data = {
             "title": "",
             "description": "Empty title"
         }
-        response = client.put("/api/tasks/27/", data=data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=data, content_type='application/json')
 
         assert response.status_code == 400
         assert "error" in response.json()
         assert "title" in response.json()["error"][0]["loc"]
 
-    def test_update_task_invalid_data_type(self, client):
+    def test_update_task_invalid_data_type(self, client, create_task):
         """Test case for when the 'title' field has an invalid data type."""
+        task_id = create_task["id"]
         data = {
             "title": 123,  # Invalid type for title
             "description": "Invalid data type for title"
         }
-        response = client.put("/api/tasks/28/", data=data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=data, content_type='application/json')
 
         assert response.status_code == 200  # HTTP 201 Created
         response_data = json.loads(response.content)
@@ -54,26 +58,28 @@ class TestUpdateTask:
         assert response_data['data']['title'] == str(data['title'])  # check if title is converted to string
         assert response_data['data']['description'] == data['description']
 
-    def test_update_task_too_long_fields(self, client):
+    def test_update_task_too_long_fields(self, client, create_task):
         """Test case for when the 'title' field exceeds the maximum allowed length."""
+        task_id = create_task["id"]
         data = {
             "title": "A" * 256,  # Assuming the max length for title is 255 characters
             "description": "A valid description"
         }
-        response = client.put("/api/tasks/29/", data=data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=data, content_type='application/json')
 
         assert response.status_code == 400
         assert "error" in response.json()
         assert "title" in response.json()["error"][0]["loc"]
 
-    def test_update_task_extra_fields(self, client):
+    def test_update_task_extra_fields(self, client, create_task):
         """Test case for when extra fields are passed in the request."""
+        task_id = create_task["id"]
         data = {
             "title": "Test Task",
             "description": "A valid test description",
             "extra_field": "This should be ignored"
         }
-        response = client.put("/api/tasks/32/", data=data, content_type='application/json')
+        response = client.put(f"/api/tasks/{task_id}/", data=data, content_type='application/json')
 
         assert response.status_code == 200  # Assuming update is successful
         assert "data" in response.json()
@@ -85,7 +91,7 @@ class TestUpdateTask:
             "title": "Test Task",
             "description": "A valid description"
         }
-        response = client.put("/api/tasks/9999/", data=data, content_type='application/json')  # Assume 9999 does not exist
+        response = client.put("/api/tasks/invalid_id/", data=data)  # Assume 9999 does not exist
 
         assert response.status_code == 404
         assert "error" in response.json()  # Error details in 'error'
@@ -97,7 +103,7 @@ class TestUpdateTask:
             "title": "Test Task",
             "description": "A valid description"
         }
-        response = client.put("/api/tasks/9999/", data=data, content_type='application/json')  # Assume 9999 does not exist
+        response = client.put("/api/tasks/999999/", data=data, content_type='application/json')  # Assume 9999 does not exist
 
         assert response.status_code == 404
         assert "error" in response.json()  # Error details in 'error'
